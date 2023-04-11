d7b30e101b4cbf54f284b3348edbee0389a9cf20
Shabeersha-Tom <102294384+Shabeersha-Tom@users.noreply.github.com>
2023-04-11 15:03:15 +0400

    Update CalculationHelpers.php

--- app/Helpers/CalculationHelpers.php
+++ app/Helpers/CalculationHelpers.php
@@ -255,5 +255,7 @@ function secondHighest($arry)
 {
     $numbers = array_unique($arry);
     rsort($numbers);
-    return $numbers[1];
+    // if (count($numbers) > 1) {
+    // }
+    return $numbers[1] ?? $numbers[0];
 }
